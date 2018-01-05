abba9558047c42be0c40f31be08e0e31ced4658b
Pedro Gonzalez C <pgonzalez58@gmail.com>
2018-01-05 18:12:02 -0400

    login modificado

--- client/pages/login/login.js
+++ client/pages/login/login.js
@@ -2,68 +2,82 @@
 	setTimeout(function () {
 		console.log($("#Enviar"));
 		$("#Enviar").click(function () {
-			login();
+			usrLogin();
 		});
 		$("#Registrar").click(function () {
-			registro();
+			usrRegistro();
 		});
 	}, 2000);
-
-	function login() {
-		$.ajax("/api/Usuarios/login", {  // ruta relativa a las api del modelo LoopBack API Explorer ver Curl
-			//$.ajax("http://localhost:3000/login", {
-			//data: { Email1: $('#Email1').val(), Password1: $('#Password1').val() },
-			//ya data se le pasan dirctamente los parametros definidos en el modelo de la app
-			//segun node_modules/loopback/common/models/user.json
-			data: { email: $('#Email1').val(), password: $('#Password1').val() },
-			method: "POST",
-			dataType: 'json',
-			success: function (data) {
-				console.log(data);
-				
-				//	if (data.statusCode == 401 || data.statusCode == 400) en caso de que se tenga el error visualizado
-				//no es muy usual ya que no se maneja todos los codigos de errores
-				if (data.error) // en este caso el error no es null es decir siempre retorna un valor, su evaluacion sera tomada para la condicion
-					alert("Usuario o Password incorrecto")
-				else {
-					//location = "http://localhost:3000/success.html"
-					console.log(data);
-					localStorage.userId=data.userId;
-					localStorage.accessToken=data.id;
-					$("#pages_container").load("/pages/success/success.html");
-
-				}
-
-			},
-			error: function (data) {
-				//console.log(data);
-				alert("Usuario o Password incorrecto")
-			},
-		});
+	function usrLogin() {
+		login({ email: $('#Email1').val(), password: $('#Password1').val() }, cbLogin);
 	}
-	function registro() {
-		$.ajax("/api/Usuarios", {
-			data: { email: $('#Email1').val(), password: $('#Password1').val() },
-			method: "POST",
-			dataType: 'json',
-			success: function (data) {
-				console.log(data);
-				
-				//	if (data.statusCode == 401 || data.statusCode == 400) en caso de que se tenga el error visualizado
-				//no es muy usual ya que no se maneja todos los codigos de errores
-				if (data.error) // en este caso el error no es null es decir siempre retorna un valor, su evaluacion sera tomada para la condicion
-					alert(data.responseJSON.error.message)
-				else
-					//location = "http://localhost:3000/success.html"
-					console.log(data);
-			},
-			error: function (data) {
-				//console.log(data);
-				alert(data.responseJSON.error.message)
-			}
-		});
+	function cbLogin(ex, data) {
+		//	if (data.statusCode == 401 || data.statusCode == 400) en caso de que se tenga el error visualizado
+		//no es muy usual ya que no se maneja todos los codigos de errores
+		if (ex) // en este caso el error no es null es decir siempre retorna un valor, su evaluacion sera tomada para la condicion
+			alert("Usuario o Password incorrecto")
+		else {
+			//location = "http://localhost:3000/success.html"
+			console.log(data);
+			localStorage.userId = data.userId; //guarda en el objeto del navegador 
+			localStorage.accessToken = data.id;
+			$("#pages_container").load("/pages/success/success.html");
+		}
 	}
 
+	function usrRegistro() {
+		registro({ email: $('#Email1').val(), password: $('#Password1').val() }, cbRegistro);
+	}
 
-}
-	)();
+	function cbRegistro(ex, data) {
+		console.log(data);
+				
+		//	if (data.statusCode == 401 || data.statusCode == 400) en caso de que se tenga el error visualizado
+		//no es muy usual ya que no se maneja todos los codigos de errores
+		if (data.error) // en este caso el error no es null es decir siempre retorna un valor, su evaluacion sera tomada para la condicion
+			alert(data.responseJSON.error.message)
+		else
+			//location = "http://localhost:3000/success.html"
+			console.log(data);
+	}
+})();
+	
+// success: function (data) {
+// 				console.log(data);
+// 				
+// 				//	if (data.statusCode == 401 || data.statusCode == 400) en caso de que se tenga el error visualizado
+// 				//no es muy usual ya que no se maneja todos los codigos de errores
+// 				if (data.error) // en este caso el error no es null es decir siempre retorna un valor, su evaluacion sera tomada para la condicion
+// 					alert("Usuario o Password incorrecto")
+// 				else {
+// 					//location = "http://localhost:3000/success.html"
+// 					console.log(data);
+// 					localStorage.userId=data.userId;
+// 					localStorage.accessToken=data.id;
+// 					$("#pages_container").load("/pages/success/success.html");
+// 
+// 				}
+// 
+// 			},
+// 			error: function (data) {
+// 				//console.log(data);
+// 				alert("Usuario o Password incorrecto")
+// 			},
+// 		});
+
+// success: function (data) {
+// 				console.log(data);
+// 				
+// 				//	if (data.statusCode == 401 || data.statusCode == 400) en caso de que se tenga el error visualizado
+// 				//no es muy usual ya que no se maneja todos los codigos de errores
+// 				if (data.error) // en este caso el error no es null es decir siempre retorna un valor, su evaluacion sera tomada para la condicion
+// 					alert(data.responseJSON.error.message)
+// 				else
+// 					//location = "http://localhost:3000/success.html"
+// 					console.log(data);
+// 			},
+// 			error: function (data) {
+// 				//console.log(data);
+// 				alert(data.responseJSON.error.message)
+// 			}
+// 		});
